f2cfc26dbef584560fe12fa745d67c02b5d08b49
Ana Beatriz Medeiros Teixeira <ana.b.teixeiramedeiros@gmail.com>
2026-06-16 13:54:25 -0300

    Feat/new adoption workflow

--- backend/routers/AdoptionRouters.js
+++ backend/routers/AdoptionRouters.js
@@ -1,8 +1,15 @@
 const router = require('express').Router()
+ const rateLimit = require('express-rate-limit')
 const AdoptionController = require('../src/adapters/controllers/AdoptionController')
 const verifyToken = require('../helpers/check-token')
 
-router.post('/request', verifyToken, AdoptionController.request)
-router.patch('/review/:id', verifyToken, AdoptionController.review)
+const adoptionLimiter = rateLimit({
+   windowMs: 15 * 60 * 1000,
+   max: 100,
+   standardHeaders: true,
+   legacyHeaders: false
+ })
+ router.post('/request', adoptionLimiter, verifyToken, AdoptionController.request)
+ router.patch('/review/:id', adoptionLimiter, verifyToken, AdoptionController.review)
 
 module.exports = router
